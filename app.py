688d3ddfc903aa954688b27264dd1840ea33c88d
ljy <1441643798@qq.com>
2024-11-30 19:10:47 +0800

    fix: 删除不必要的页面

--- app.py
+++ app.py
@@ -7,29 +7,45 @@ import torch
 import pandas as pd
 import config
 import models
+import configparser
 
-from flask import Flask, redirect, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from PIL import Image
 from werkzeug.utils import secure_filename
 from ext import db
 from flask_migrate import Migrate
 
+app = Flask(__name__)
+
+# 配置数据库
+app.json.ensure_ascii = False
+app.config.from_object(config)
+db.init_app(app)
+migrate = Migrate(app, db)
+
+env = configparser.ConfigParser()
+env.read('env.ini')
 
 # 导入csv数据
 disease_info = pd.read_csv('disease_info.csv', encoding='utf-8')
-supplement_info = pd.read_csv('supplement_info.csv',encoding='utf-8')
+supplement_info = pd.read_csv('supplement_info.csv', encoding='utf-8')
 
 # 加载模型权重
-model = CNN.CNN(39)    
-model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+model = CNN.CNN(39)
+model.load_state_dict(torch.load("Ljy-Plant-Model.pt"))
 model.eval()
 
+host = env['flask']['host']
+port = env.getint('flask', 'port')
+
 # 允许上传的图片格式
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 #  预训练
 def prediction(image_path):
@@ -42,37 +58,8 @@ def prediction(image_path):
     index = np.argmax(output)
     return index
 
-app = Flask(__name__)
-
-# 配置数据库
-app.json.ensure_ascii = False # 解决中文乱码问题
-app.config.from_object(config)
-db.init_app(app)
-migrate = Migrate(app, db)
 
 # 接口
-@app.route('/')
-def home_page():
-    return render_template('home.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact-us.html')
-
-@app.route('/index')
-def ai_engine_page():
-    return render_template('index.html')
-
-@app.route('/mobile-device')
-def mobile_device_detected_page():
-    return render_template('mobile-device.html')
-
-
-@app.route('/market', methods=['GET', 'POST'])
-def market():
-    return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
-                           supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
-
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
@@ -96,6 +83,7 @@ def submit():
             return jsonify(response_data)
     return jsonify({'error': 'Invalid request or no file provided'}), 400
 
+
 @app.route('/plant/list')
 def science():
     page = request.args.get('page', 1, type=int)
@@ -106,8 +94,6 @@ def science():
         query = query.filter(models.ScienceModel.sub_type == nid)
         pagination = query.paginate(page=page, per_page=per_page, error_out=False)
         sciences = pagination.items
-    # pagination = models.ScienceModel.query.filter_by(sub_type=nid).paginate(page = page, per_page = per_page)
-    # sciences = pagination.items
     # 构建响应数据
     response_data = {
         'code': 0,
@@ -129,6 +115,7 @@ def science():
     }
     return jsonify(response_data)
 
+
 @app.route('/plant/detail')
 def science_detail():
     science_id = request.args.get('id')
@@ -138,10 +125,9 @@ def science_detail():
     # 检查科学模型是否存在
     if science is None:
         return jsonify({'code': 1, 'message': 'Science not found'}), 404
-    # science = models.ScienceModel.query.get(science_id)
     science_data = {
         "code": 0,
-        "data":{
+        "data": {
             'id': science.id,
             'plant_id': science.plant_id,
             'name': science.name,
@@ -153,7 +139,7 @@ def science_detail():
     }
     return jsonify(science_data)
 
+
 # 启动入口
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5556, debug=True)
-
+    app.run(host=host, port=port, debug=True)  # 例如host='127.0.0.1'  port=5555
